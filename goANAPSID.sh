3e2574ef78b099443d5d6b3669cf8301a175fc4b
Simón Castillo <simoncastillo@gmail.com>
2014-04-26 10:31:47 -0430

    Fixing line breaks.

--- goANAPSID.sh
+++ goANAPSID.sh
@@ -12,16 +12,16 @@ do
     echo $endpoint >> $RESULTS_FILE
     for query in $(ls $QUERY_ROOT/owlim/$endpoint/*.sparql)
     do
-	(timeout -s 12 $TIMEOUT python $CMD_FILE
-            -e $QUERY_ROOT/owlim/$endpoint/endpoint.anapsid
-            -q $query
-            -p b
-            -s False
-            -b 16384
-            -o False
-            -d SSGM
-            -a True
-            -w True
+	(timeout -s 12 $TIMEOUT python $CMD_FILE \
+            -e $QUERY_ROOT/owlim/$endpoint/endpoint.anapsid \
+            -q $query \
+            -p b \
+            -s False \
+            -b 16384 \
+            -o False \
+            -d SSGM \
+            -a True \
+            -w True \
             -t O)
         2>> $ERRORS_FILE
         >> $RESULTS_FILE;
@@ -33,16 +33,16 @@ do
     echo $endpoint >> $RESULTS_FILE
     for query in $(ls $QUERY_ROOT/virtuoso/$endpoint/*.sparql)
     do
-	(timeout -s 12 $TIMEOUT python $CMD_FILE
-            -e $QUERY_ROOT/virtuoso/$endpoint/endpoint.anapsid
-            -q $query
-            -p b
-            -s False
-            -b 16384
-            -o False
-            -d SSGM
-            -a True
-            -w True
+	(timeout -s 12 $TIMEOUT python $CMD_FILE \
+            -e $QUERY_ROOT/virtuoso/$endpoint/endpoint.anapsid \
+            -q $query \
+            -p b \
+            -s False \
+            -b 16384 \
+            -o False \
+            -d SSGM \
+            -a True \
+            -w True \
             -t V)
         2>> $ERRORS_FILE
         >> $RESULTS_FILE;
