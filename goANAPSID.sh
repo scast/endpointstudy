65e0aed5920eba8ba63ec799eb987325881cc45b
Simón Castillo <simoncastillo@gmail.com>
2014-04-26 10:27:46 -0430

    no random variables

--- goANAPSID.sh
+++ goANAPSID.sh
@@ -5,13 +5,14 @@ EXPERIMENTS_ROOT=$ROOT/results
 RESULTS_FILE=$EXPERIMENTS_ROOT/anapsid-results-$(date +"%Y%m%d-%k:%M:%S")
 ERRORS_FILE=$EXPERIMENTS_ROOT/anapsid-errors-$(date +"%Y%m%d-%k:%M:%S")
 CMD_FILE=$ROOT/bin/runAnapsid.py
+TIMEOUT=100
 echo $RESULTS_FILE;
 for endpoint in $(ls $QUERY_ROOT/virtuoso)
 do
     echo $endpoint >> $RESULTS_FILE
     for query in $(ls $QUERY_ROOT/virtuoso/$endpoint/*.sparql)
     do
-	(timeout -s 12 100 python $CMD_FILE
+	(timeout -s 12 $TIMEOUT python $CMD_FILE
             -e $QUERY_ROOT/virtuoso/$endpoint/endpoint.anapsid
             -q $query
             -p b
@@ -32,7 +33,7 @@ do
     echo $endpoint >> $RESULTS_FILE
     for query in $(ls $QUERY_ROOT/owlim/$endpoint/*.sparql)
     do
-	(timeout -s 12 100 python $CMD_FILE
+	(timeout -s 12 $TIMEOUT python $CMD_FILE
             -e $QUERY_ROOT/owlim/$endpoint/endpoint.anapsid
             -q $query
             -p b
