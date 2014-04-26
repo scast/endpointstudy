b9666e5a582da3d4c38047eb4d1cf3bd1d73c92f
Simón Castillo <simoncastillo@gmail.com>
2014-04-26 10:29:52 -0430

    Reshuffling.

--- goANAPSID.sh
+++ goANAPSID.sh
@@ -7,27 +7,6 @@ ERRORS_FILE=$EXPERIMENTS_ROOT/anapsid-errors-$(date +"%Y%m%d-%k:%M:%S")
 CMD_FILE=$ROOT/bin/runAnapsid.py
 TIMEOUT=100
 echo $RESULTS_FILE;
-for endpoint in $(ls $QUERY_ROOT/virtuoso)
-do
-    echo $endpoint >> $RESULTS_FILE
-    for query in $(ls $QUERY_ROOT/virtuoso/$endpoint/*.sparql)
-    do
-	(timeout -s 12 $TIMEOUT python $CMD_FILE
-            -e $QUERY_ROOT/virtuoso/$endpoint/endpoint.anapsid
-            -q $query
-            -p b
-            -s False
-            -b 16384
-            -o False
-            -d SSGM
-            -a True
-            -w True
-            -t V)
-        2>> $ERRORS_FILE
-        >> $RESULTS_FILE;
-    done
-done
-
 for endpoint in $(ls $QUERY_ROOT/owlim)
 do
     echo $endpoint >> $RESULTS_FILE
@@ -44,6 +23,27 @@ do
             -a True
             -w True
             -t O)
+        2>> $ERRORS_FILE
+        >> $RESULTS_FILE;
+    done
+done
+
+for endpoint in $(ls $QUERY_ROOT/virtuoso)
+do
+    echo $endpoint >> $RESULTS_FILE
+    for query in $(ls $QUERY_ROOT/virtuoso/$endpoint/*.sparql)
+    do
+	(timeout -s 12 $TIMEOUT python $CMD_FILE
+            -e $QUERY_ROOT/virtuoso/$endpoint/endpoint.anapsid
+            -q $query
+            -p b
+            -s False
+            -b 16384
+            -o False
+            -d SSGM
+            -a True
+            -w True
+            -t V)
         2>> $ERRORS_FILE
         >> $RESULTS_FILE;
     done
